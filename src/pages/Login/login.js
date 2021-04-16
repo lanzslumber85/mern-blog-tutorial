43ca8e2c30ebe78b6d6233e5f40d1cd1f1ee599e
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-16 09:26:05 +0800

    Buat Login page

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,11 +1,91 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Login = () => {
-    return (
-        <>
-            <h1>Login Page</h1>
-        </>
-    );
-};
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = async event => {
+        await this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    submitForm(e) {
+        e.preventDefault();
+        console.log(`Email: ${this.state.email}`);
+        console.log(`Password: ${this.state.password}`);
+    }
+
+    render() {
+        const { email, password } = this.state;
+
+        return (
+            <div>
+                <form>
+                    <h3>Login</h3>
+
+                    <div className="form-group">
+                        <label htmlFor="email">Email address</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="form-control"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={e => this.handleChange(e)}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="form-control"
+                            placeholder="Enter password"
+                            value={password}
+                            onChange={e => this.handleChange(e)}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input
+                                type="checkbox"
+                                className="custom-control-input"
+                                id="customCheck1"
+                            />
+                            <label
+                                className="custom-control-label"
+                                htmlFor="customCheck1"
+                            >
+                                Remember me
+                            </label>
+                        </div>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary btn-block"
+                        onClick={e => this.submitForm(e)}
+                    >
+                        Submit
+                    </button>
+                    <p className="forgot-password text-right">
+                        Forgot password?
+                    </p>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Login;
--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -11,7 +11,6 @@ import {
     FormText,
     FormFeedback,
 } from "reactstrap";
-import "../../assets/css/register.css";
 
 class Register extends Component {
     constructor() {
@@ -60,88 +59,90 @@ class Register extends Component {
     render() {
         const { email, password, password2, fullName } = this.state;
         return (
-            <Container className="App">
-                <h2>Register</h2>
-                <Form className="form" onSubmit={e => this.submitForm(e)}>
-                    <Col>
-                        <FormGroup>
-                            <Label for="fullName">Full Name</Label>
-                            <Input
-                                type="text"
-                                name="fullName"
-                                id="fullName"
-                                placeholder="********"
-                                value={fullName}
-                                onChange={e => this.handleChange(e)}
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Col>
-                        <FormGroup>
-                            <Label>Username</Label>
-                            <Input
-                                type="email"
-                                name="email"
-                                id="exampleEmail"
-                                placeholder="myemail@email.com"
-                                value={email}
-                                valid={
-                                    this.state.validate.emailState ===
-                                    "has-success"
-                                }
-                                invalid={
-                                    this.state.validate.emailState ===
-                                    "has-danger"
-                                }
-                                onChange={e => {
-                                    this.validateEmail(e);
-                                    this.handleChange(e);
-                                }}
-                            />
-                            <FormFeedback valid>
-                                That's a tasty looking email you've got there.
-                            </FormFeedback>
-                            <FormFeedback>
-                                Uh oh! Looks like there is an issue with your
-                                email. Please input a correct email.
-                            </FormFeedback>
-                            <FormText>
-                                Your username is most likely your email.
-                            </FormText>
-                        </FormGroup>
-                    </Col>
-                    <Col>
-                        <FormGroup>
-                            <Label for="examplePassword">Password</Label>
-                            <Input
-                                type="password"
-                                name="password"
-                                id="examplePassword"
-                                placeholder="********"
-                                value={password}
-                                onChange={e => this.handleChange(e)}
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Col>
-                        <FormGroup>
-                            <Label for="examplePassword2">
-                                Confirm Password
-                            </Label>
-                            <Input
-                                type="password"
-                                name="password2"
-                                id="examplePassword2"
-                                placeholder="********"
-                                value={password2}
-                                onChange={e => this.handleChange(e)}
-                            />
-                        </FormGroup>
-                    </Col>
-                    <Button>Register</Button>
-                </Form>
-                <Link to="/login">Back to login page</Link>
-            </Container>
+            <div>
+                <Container className="App">
+                    <h3>Register</h3>
+                    <Form className="form" onSubmit={e => this.submitForm(e)}>
+                        <Col>
+                            <FormGroup>
+                                <Label for="fullName">Full Name</Label>
+                                <Input
+                                    type="text"
+                                    name="fullName"
+                                    id="fullName"
+                                    value={fullName}
+                                    onChange={e => this.handleChange(e)}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col>
+                            <FormGroup>
+                                <Label>Username</Label>
+                                <Input
+                                    type="email"
+                                    name="email"
+                                    id="exampleEmail"
+                                    placeholder="myemail@email.com"
+                                    value={email}
+                                    valid={
+                                        this.state.validate.emailState ===
+                                        "has-success"
+                                    }
+                                    invalid={
+                                        this.state.validate.emailState ===
+                                        "has-danger"
+                                    }
+                                    onChange={e => {
+                                        this.validateEmail(e);
+                                        this.handleChange(e);
+                                    }}
+                                />
+                                <FormFeedback valid>
+                                    That's a tasty looking email you've got
+                                    there.
+                                </FormFeedback>
+                                <FormFeedback>
+                                    Uh oh! Looks like there is an issue with
+                                    your email. Please input a correct email.
+                                </FormFeedback>
+                                <FormText>
+                                    Your username is most likely your email.
+                                </FormText>
+                            </FormGroup>
+                        </Col>
+                        <Col>
+                            <FormGroup>
+                                <Label for="examplePassword">Password</Label>
+                                <Input
+                                    type="password"
+                                    name="password"
+                                    id="examplePassword"
+                                    placeholder="********"
+                                    value={password}
+                                    onChange={e => this.handleChange(e)}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col>
+                            <FormGroup>
+                                <Label for="examplePassword2">
+                                    Confirm Password
+                                </Label>
+                                <Input
+                                    type="password"
+                                    name="password2"
+                                    id="examplePassword2"
+                                    placeholder="********"
+                                    value={password2}
+                                    onChange={e => this.handleChange(e)}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Button>Register</Button>
+                    </Form>
+                    <Link to="/login">Back to login page</Link>
+                </Container>
+            </div>
         );
     }
 }
