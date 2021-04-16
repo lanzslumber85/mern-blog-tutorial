baad492b14aaa792890fa3ddcfa36dd044f56bcf
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-17 02:23:20 +0800

    Install cors and apply it

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const productRoutes = require("./src/routes/productRoutes");
 
 const app = express();
 
-app.use("/", productRoutes);
+app.use(cors());
+
+app.use("/v1/customer", productRoutes);
 
 app.listen(4000);
