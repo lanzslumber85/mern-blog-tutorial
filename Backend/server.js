d062e11649d783f90917a113f45485ab3e3c63b0
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-17 15:08:13 +0800

    Update changes

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,6 @@ app.use(cors());
 app.use("/v1/auth", authRoutes);
 app.use("/v1/blog", blogRoutes);
 
-app.listen(4000, () =>
-    console.log("Server is up and listening at port 4000...")
-);
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is listening at port ${port}...`));
