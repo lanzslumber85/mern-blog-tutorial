a6f50ee0bc3daecff3068ad098131517182583a2
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-20 07:04:31 +0800

    Replacing dummy data with response API data

--- src/components/blog/blogComponent.js
+++ src/components/blog/blogComponent.js
@@ -2,32 +2,25 @@ import React from "react";
 import { useHistory } from "react-router";
 import { Button } from "reactstrap";
 
-const BlogComponent = () => {
+const BlogComponent = props => {
     const history = useHistory();
     return (
         <div>
             <div>
-                <img src="http://placeimg.com/240/180/tech" alt="dummy"></img>
+                <img src={props.image} alt="blogpost" />
             </div>
             <div>
-                <h5>Title Blog</h5>
+                <h5>{props.title}</h5>
             </div>
             <div>
                 <p>
-                    <small>Author</small>
+                    <small>{props.authorName}</small>
                     <span> - </span>
-                    <small>Date post</small>
+                    <small>{props.datePost}</small>
                 </p>
             </div>
             <div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Duis accumsan velit dui, ut lobortis sapien maximus eu.
-                    Proin lobortis placerat mi, sed interdum tortor mattis a.
-                    Aliquam cursus ultricies enim. Suspendisse commodo orci
-                    vitae nibh semper, ut dapibus orci pharetra. Nam condimentum
-                    velit elit, non efficitur arcu efficitur eu. Sed.
-                </p>
+                <p>{props.content}</p>
             </div>
             <div>
                 <Button onClick={() => history.push("/detail-blog")}>
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "reactstrap";
 import BlogComponent from "../../components/blog/blogComponent";
 import { useHistory } from "react-router";
 import Axios from "axios";
 
 const Home = () => {
+    const [blogItems, setBlogItems] = useState([]);
+
     useEffect(() => {
-        Axios.get("http://localhost:4000/v1/blog/get-blogs")
+        Axios.get("http://localhost:4000/v1/blog/get-blogs?page=2")
             .then(result => {
                 console.log("data API? ", result);
+                const responseAPI = result.data;
+                setBlogItems(responseAPI.data);
             })
             .catch(err => {
                 console.log("error? ", err);
@@ -23,12 +27,19 @@ const Home = () => {
             <Button onClick={() => history.push("/create-blog")}>
                 Create Blog
             </Button>
-            <BlogComponent />
-            <BlogComponent />
-            <BlogComponent />
-            <BlogComponent />
-            <BlogComponent />
-            <BlogComponent />
+            {blogItems.map(item => {
+                return (
+                    <BlogComponent
+                        key={item._id}
+                        image={`http://localhost:4000/${item.image}`}
+                        title={item.title}
+                        content={item.content}
+                        authorName={item.author.name}
+                        datePost={item.createdAt}
+                    />
+                );
+            })}
+
             <Button>Previous</Button>
             <Button>Next</Button>
         </div>
