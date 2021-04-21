c02e9b3e3ad50be327b27079be4b3486dca6fbe2
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-21 08:03:33 +0800

    Setup redux

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,13 @@
+import { Provider } from "react-redux";
 import FirstRoutes from "../config/Routers/first";
+import store from "../redux/store/store";
 
 const App = () => {
-    return <FirstRoutes />;
+  return (
+    <Provider store={store}>
+      <FirstRoutes />
+    </Provider>
+  );
 };
 
 export default App;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -3,47 +3,48 @@ import { Button } from "reactstrap";
 import BlogComponent from "../../components/blog/blogComponent";
 import { useHistory } from "react-router";
 import Axios from "axios";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-    const [blogItems, setBlogItems] = useState([]);
+  const [blogItems, setBlogItems] = useState([]);
 
-    useEffect(() => {
-        Axios.get("http://localhost:4000/v1/blog/get-blogs?page=2")
-            .then(result => {
-                console.log("data API? ", result);
-                const responseAPI = result.data;
-                setBlogItems(responseAPI.data);
-            })
-            .catch(err => {
-                console.log("error? ", err);
-            });
-    }, []);
+  const stateGlobal = useSelector(state => state);
 
-    const history = useHistory();
+  useEffect(() => {
+    Axios.get("http://localhost:4000/v1/blog/get-blogs?page=2")
+      .then(result => {
+        // console.log("data API? ", result);
+        const responseAPI = result.data;
+        setBlogItems(responseAPI.data);
+      })
+      .catch(err => {
+        console.log("error? ", err);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h3>Home Page</h3>
-            <Button onClick={() => history.push("/create-blog")}>
-                Create Blog
-            </Button>
-            {blogItems.map(item => {
-                return (
-                    <BlogComponent
-                        key={item._id}
-                        image={`http://localhost:4000/${item.image}`}
-                        title={item.title}
-                        content={item.content}
-                        authorName={item.author.name}
-                        datePost={item.createdAt}
-                    />
-                );
-            })}
+  const history = useHistory();
 
-            <Button>Previous</Button>
-            <Button>Next</Button>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Home Page</h3>
+      <Button onClick={() => history.push("/create-blog")}>Create Blog</Button>
+      {blogItems.map(item => {
+        return (
+          <BlogComponent
+            key={item._id}
+            image={`http://localhost:4000/${item.image}`}
+            title={item.title}
+            content={item.content}
+            authorName={item.author.name}
+            datePost={item.createdAt}
+          />
+        );
+      })}
+
+      <Button>Previous</Button>
+      <Button>Next</Button>
+    </div>
+  );
 };
 
 export default Home;
